975b3cb7709e78069cb143390836114f0d136715
kurad <106243129+kuradai33@users.noreply.github.com>
2025-11-05 10:29:47 +0900

    結果の表示を追加

--- src/pages/ResultPage.ts
+++ src/pages/ResultPage.ts
@@ -21,19 +21,47 @@ export default class Resultpage extends Page {
 
                 <!-- メトリクス（任意数のKPIを自由に表示） -->
                 <h3>メトリクス</h3>
-                <table id="result-metrics">
+                <table id="result_metrics">
                     <thead>
-                        <tr><th>項目</th><th>値</th></tr>
+                        <tr>
+                            <th>順位</th>
+                            <th>名前</th>
+                            <th colspan="6">タイム</th>
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th>合計</th>
+                            <th>ラウンド１</th>
+                            <th>ラウンド２</th>
+                            <th>ラウンド３</th>
+                            <th>ラウンド４</th>
+                            <th>ラウンド５</th>
+                        </tr>
                     </thead>
-                    <tbody>
-                        <!-- 動的に挿入 -->
+                    <tbody id="result_metrics_body">
+                        <tr>
+                            <td>1</td>
+                            <td>hoge</td>
+                            <td>5:00.00</td>
+                            <td>1:00.00</td>
+                            <td>1:00.00</td>
+                            <td>1:00.00</td>
+                            <td>1:00.00</td>
+                            <td>1:00.00</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>hoge2</td>
+                            <td>10:00.00</td>
+                            <td>2:00.00</td>
+                            <td>2:00.00</td>
+                            <td>2:00.00</td>
+                            <td>2:00.00</td>
+                            <td>2:00.00</td>
+                        </tr>
                     </tbody>
                 </table>
-
-                <!-- セクション（任意数、配列で増減可能） -->
-                <div id="result-sections">
-                    <!-- 動的に <article> や <details> を追加 -->
-                </div>
 
                 <footer style="margin-top:1rem;">
                     <button type="button" id="btn_retry">もう一度</button>
@@ -54,5 +82,33 @@ export default class Resultpage extends Page {
         };
         const btnBackTitle = document.getElementById("btn_backtitle") as HTMLButtonElement;
         btnBackTitle.addEventListener("click", actionBackTitle);
+
+        const results = [...manager.state.results].sort((resultX, resultY) => resultX.totalTimeMs - resultY.totalTimeMs);
+        const tbhtml = results.map((result, idx) => {
+            let detailTime = "";
+            const len = result.timeMsByRound.length;
+            for (let i = 0; i < 5; i++) {
+                detailTime += `<td>${i < len ? Resultpage.format(result.timeMsByRound[i]) : ""}</td>`;
+            }
+            return `<tr>` +
+                    `<td>${idx + 1}</td>` +
+                    `<td>${result.name}</td>` +
+                    `<td>${Resultpage.format(result.totalTimeMs)}</td>` +
+                    `${detailTime}` +
+                `</tr>`;
+        });
+        const resultTableBody = document.getElementById("result_metrics_body") as HTMLTableElement;
+        console.log(tbhtml);
+        resultTableBody.innerHTML = tbhtml.join("");
+    }
+
+    static format(elapsedMs: number): string {
+        const cs = Math.floor(elapsedMs / 10) % 100; // centiseconds
+        const s = Math.floor(elapsedMs / 1000) % 60;
+        const m = Math.floor(elapsedMs / 60000);
+        if (m > 99) return "99:99.99";
+
+        const pad2 = (n: number) => (n < 10 ? "0" + n : "" + n);
+        return m + ":" + pad2(s) + "." + pad2(cs);
     }
 }
